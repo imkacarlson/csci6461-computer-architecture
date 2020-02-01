779b67e7ca983fbbe987c5deecb7526be241b671
Keith Carlson <imkacarlson@gmail.com>
2020-02-01 14:44:19 -0500

    Blank window created
    using windowbuilder

---
+++ csci6534/MyPC.java
@@ -0,0 +1,43 @@
+package csci6534;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MyPC {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyPC window = new MyPC();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MyPC() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 758, 467);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
